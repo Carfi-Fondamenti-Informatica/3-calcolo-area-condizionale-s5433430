9810420d9f750e58a6ded930f93559f67bdd6628
s5433430 <115527969+s5433430@users.noreply.github.com>
2022-11-17 21:28:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   float a = 0, b = 0;
+   Int opzione = 0;
+   cin >> a >> b;
+   cin >> opzione;
+   
+   switch (opzione) {
+      case 1: cout << (a * b/2) << endl;
+         break;
+      case 2: cout << (a * a) << endl;
+         break;
+      case 3: cout << (a * b) << endl;
+         break;
+      default: cout << "opzione non valida" << endl;
+   }
    return 0;
 }
